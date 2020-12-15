f6747d8b3eedb687d910119e51805b1d5ff5bd13
Kamil <kamil.mikus@onet.pl>
2020-12-15 21:58:32 +0100

    added EF entities configuration

---
+++ TodoListApp/TodoListApp.Persistance/Configurations/SingleTaskConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using TodoListApp.Core.Domain;
+
+namespace TodoListApp.Persistance.Configurations
+{
+    public class SingleTaskConfiguration : IEntityTypeConfiguration<SingleTask>
+    {
+        public void Configure(EntityTypeBuilder<SingleTask> builder)
+        {
+            builder.ToTable("Tasks");
+
+            builder.HasKey(x => x.SingleTaskId);
+
+            builder.Property(x => x.TaskName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.Priority)
+                .IsRequired()
+                .HasConversion(new EnumToNumberConverter<PriorityLevel, byte>())
+                .HasDefaultValue(PriorityLevel.Low);
+
+            builder.Property(x => x.CreatedAt)
+                .IsRequired();
+
+            builder.HasOne(x => x.Board)
+                .WithMany(x => x.Tasks);
+        }
+    }
+}
---
+++ TodoListApp/TodoListApp.Persistance/Configurations/TaskBoardConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TodoListApp.Core.Domain;
+
+namespace TodoListApp.Persistance.Configurations
+{
+    public class TaskBoardConfiguration : IEntityTypeConfiguration<TasksBoard>
+    {
+        public void Configure(EntityTypeBuilder<TasksBoard> builder)
+        {
+            builder.HasKey(x => x.TasksBoardId);
+
+            builder.Property(x => x.CategoryName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.Boards);
+        }
+    }
+}
