d96ced0fa04248610cfb6bc189aaa016a0e69117
Kamil <kamil.mikus@onet.pl>
2020-12-17 10:57:58 +0100

    unitOfWork added

---
+++ TodoListApp/TodoListApp.Core/DomainAccessAbstraction/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Core.DomainAccessAbstraction
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        Task<int> Complete();
+    }
+}
