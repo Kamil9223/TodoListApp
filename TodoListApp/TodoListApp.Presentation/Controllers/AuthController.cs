4b5d378c63f48691096ed1485e31b1d50e5a9be0
Kamil <kamil.mikus@onet.pl>
2020-12-17 11:59:27 +0100

    added auth controllers with views

---
+++ TodoListApp/TodoListApp.Presentation/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListApp.Presentation.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(string userName, string password)
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string userName, string password)
+        {
+            return View();
+        }
+    }
+}
