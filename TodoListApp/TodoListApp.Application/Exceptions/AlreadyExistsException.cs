7b36716777dcb8516989cba83f23f45241d53e22
Kamil <kamil.mikus@onet.pl>
2021-01-14 17:17:40 +0100

    add application exceptions

---
+++ TodoListApp/TodoListApp.Application/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TodoListApp.Application.Exceptions
+{
+    public class AlreadyExistsException : Exception
+    {
+        private const string displayMessage = "Already exists";
+
+        private string _logMessage;
+
+        public AlreadyExistsException() : base(displayMessage)
+        { }
+
+        public AlreadyExistsException(string logMessage) : base(displayMessage)
+        {
+            _logMessage = logMessage;
+        }
+
+        public AlreadyExistsException(string message, string logMessage) : base(message)
+        {
+            _logMessage = logMessage;
+        }
+    }
+}
