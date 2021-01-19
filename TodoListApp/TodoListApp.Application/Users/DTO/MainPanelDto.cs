98fb168d27ceafc4e5becc70dcf2ab8721e5b86d
Kamil <kamil.mikus@onet.pl>
2021-01-19 13:54:13 +0100

    created use main board query (WIP)

---
+++ TodoListApp/TodoListApp.Application/Users/DTO/MainPanelDto.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using TodoListApp.Core.Domain;
+
+namespace TodoListApp.Application.Users.DTO
+{
+    public class MainPanelDto
+    {
+        public int FirstBoardId { get; set; }
+        public List<string> CategoryNames { get; set; }
+        public List<MainPanelTasksDto> Tasks { get; set; }
+
+    }
+
+    public class MainPanelTasksDto
+    {
+        public int SingleTaskId { get; set; }
+        public string TaskName { get; set; }
+        public PriorityLevel Priority { get; set; }
+        public bool PredictedBestBeforeDate { get; set; }
+    }
+}
---
+++ TodoListApp/TodoListApp.Application/Users/Queries/UserBoardQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TodoListApp.Application.Users.DTO;
+
+namespace TodoListApp.Application.Users.Queries
+{
+    public class UserBoardQueryHandler : IRequestHandler<UserBoardQuery, MainPanelDto>
+    {
+        public Task<MainPanelDto> Handle(UserBoardQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
