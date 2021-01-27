cef2c4dfd2d77bc1b39bc7864eb23dc8e410fd45
Kamil <kamil.mikus@onet.pl>
2021-01-27 18:19:16 +0100

    added sql script with sample data, fill test sqlite db before all db integration tests and clean up after all db integration tests

--- TodoListApp/TodoListApp.IntegrationTests/DatabaseIntegration/Tests/UserRepositoryTest.cs
+++ TodoListApp/TodoListApp.IntegrationTests/DatabaseIntegration/Tests/UserRepositoryTest.cs
@@ -3,39 +3,28 @@ using System;
 using System.Threading.Tasks;
 using TodoListApp.Core.Domain;
 using TodoListApp.Core.DomainAccessAbstraction;
+using TodoListApp.IntegrationTests.DatabaseIntegration.DatabaseConfiguration;
 using TodoListApp.Persistance.Context;
 using TodoListApp.Persistance.DataAccess;
 using Xunit;
 
 namespace TodoListApp.IntegrationTests.DatabaseIntegration.Tests
 {
-    public class UserRepositoryTest : TestBase, IDisposable
+    [Collection(DatabaseFixture.DbCollection)]
+    public class UserRepositoryTest
     {
-        private readonly TodoTasksContext _todoTasksContext;
+        private readonly DatabaseFixture _databaseFixture;
         private readonly IUserRepository _userRepository;
 
-        public UserRepositoryTest()
+        public UserRepositoryTest(DatabaseFixture databaseFixture)
         {
-            _todoTasksContext = CreateInMemoryDbContext();
-            _userRepository = new UserRepository(_todoTasksContext);
+            _databaseFixture = databaseFixture;
+            _userRepository = new UserRepository(_databaseFixture.TodoTasksContext);
         }
-
-        public void Dispose()
-        {
-            //TODO: Rozwiązanie tymczasowe, docelowo nalezy utworzyć bazk inMemory
-            //dla wszysktich testów inegracyjnych z bazą, dodać przykładowe dane, które
-            //symulować będą konkretny stan. (AutoFixture)
-            _todoTasksContext.Database.EnsureDeleted();
-            _todoTasksContext.Dispose();
-        }
-
 
         [Fact]
         public async Task Should_returns_user_by_email() //TODO: Refactoring
         {
-            _todoTasksContext.Users.Add(new User("testEmail", "passwordHash"));
-            await _todoTasksContext.SaveChangesAsync();
-
             var user = await _userRepository.GetByEmail("testEmail");
 
             user.Should().NotBeNull();
