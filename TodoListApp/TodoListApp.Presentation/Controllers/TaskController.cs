cc0e6955d1e133aa8340d88de469d95b100f820a
Kamil <kamil.mikus@onet.pl>
2021-02-20 16:29:18 +0100

    prepared controller and view for task details

---
+++ TodoListApp/TodoListApp.Presentation/Controllers/TaskController.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListApp.Presentation.Controllers
+{
+    [Authorize]
+    public class TaskController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public TaskController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public IActionResult TaskDetails(int id)
+        {
+            return View();
+        }
+    }
+}
